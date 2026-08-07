767aedcba0a70ce0da4eec1c9ea371ebd1c289b4
Jacob Ebey <jacob.ebey@live.com>
2026-08-07 12:51:09 -0700

    cleanup

--- drizzle/20260805232830_plain_nightmare/migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `status_table` (
-	`uri` text NOT NULL,
-	`author_did` text NOT NULL,
-	`status` text NOT NULL,
-	`created_at` text NOT NULL,
-	`indexed_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL
-);
---> statement-breakpoint
-CREATE INDEX `author_did_idx` ON `status_table` (`author_did`);
